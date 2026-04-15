4e6fd794234f663363d564efae45f564bc150380
atromcon <atromcon@gmail.com>
2026-04-15 13:16:25 +0200

    commit random

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+      public int vidas = 3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
